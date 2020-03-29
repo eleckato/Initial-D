54178cabe6ecc42957255a677af5ef195fe95974
Seba Herrera <seherreraluna@gmail.com>
2020-03-29 00:45:57 -0300

    Seteo inicial
    Avance con UI base

---
+++ initial-d/initial-d/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="initial_d.MvcApplication" Language="C#" %>
---
+++ initial-d/initial-d/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace initial_d.Models
+{
+    public class User
+    {
+
+    }
+}
